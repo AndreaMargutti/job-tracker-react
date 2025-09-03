69b4eefb931ea8b266bb5bf5260f988828aba03d
AndreaMargutti <marguttiandrea30@gmail.com>
2025-09-03 14:30:25 +0200

    Update title in index.html to "Job Tracker" and wrap Title component text in a link for improved navigation

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -5,7 +5,9 @@ type TitleProps = {
 export default function Title({ title }: TitleProps) {
   return (
     <>
-      <h1 className="text-3xl font-bold text-shadow-lg">{title}</h1>
+      <h1 className="text-3xl font-bold text-shadow-lg">
+        <a href="/">{title}</a>
+      </h1>
     </>
   );
 }
