20e633f16ef1146d846da4337e1c201562653163
AndreaMargutti <marguttiandrea30@gmail.com>
2025-09-04 23:40:17 +0200

    Add interactivity to Button component: implement selection state with toggle functionality and update styles for selected state.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import type { ButtonProps } from "./ButtonProps";
 
 export default function Button({ text }: ButtonProps) {
+  const [isSelected, setIsActive] = useState(false);
+
+  const selectedStyle = () => {
+    if (!isSelected) return "";
+
+    return "bg-[#9be3dd] text-white";
+  };
+
   return (
     <>
-      <button className="font-bold uppercase hover:underline underline-offset-4 text-[#9be3dd]">
+      <button
+        onClick={() => setIsActive((prev) => !prev)}
+        className={`border-2 rounded-lg py-2 px-4 font-bold uppercase hover:underline underline-offset-4 text-[#9be3dd] ${selectedStyle()}`}
+      >
         {text}
       </button>
     </>
