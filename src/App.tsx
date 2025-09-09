aff574bea428d7cb218b4f02dfc3e34c79977d40
AndreaMargutti <marguttiandrea30@gmail.com>
2025-09-09 17:14:57 +0200

    refactor: update layout and styling of job offers section in App component

--- src/App.tsx
+++ src/App.tsx
@@ -22,110 +22,12 @@ function App() {
           />
         </div>
         <section className="grid grid-cols-3 gap-8 px-8 pb-8">
-          <div className="max-h-[60vh] bg-red-500 overflow-scroll transparent-bar">
-            <h2 className="sticky top-0 bg-amber-400 text-center font-extrabold uppercase text-xl">
+          <div className="max-h-[60vh] border-4 rounded-md text-[#9be3dd] overflow-scroll transparent-bar">
+            <h2 className="sticky top-0 text-center pt-4 font-extrabold uppercase text-xl">
               Sent
             </h2>
             <section className="p-4">
-              <div className="bg-blue-500 mb-4">
-                <h2>TITOLO OFFERTA</h2>
-                <h4>Nome Azienda</h4>
-                <p>Luogo</p>
-                <a href="#">Altre informazioni</a>
-              </div>
-              <div className="bg-blue-500 mb-4">
-                <h2>TITOLO OFFERTA</h2>
-                <h4>Nome Azienda</h4>
-                <p>Luogo</p>
-                <a href="#">Altre informazioni</a>
-              </div>
-              <div className="bg-blue-500 mb-4">
-                <h2>TITOLO OFFERTA</h2>
-                <h4>Nome Azienda</h4>
-                <p>Luogo</p>
-                <a href="#">Altre informazioni</a>
-              </div>
-              <div className="bg-blue-500 mb-4">
-                <h2>TITOLO OFFERTA</h2>
-                <h4>Nome Azienda</h4>
-                <p>Luogo</p>
-                <a href="#">Altre informazioni</a>
-              </div>
-              <div className="bg-blue-500 mb-4">
-                <h2>TITOLO OFFERTA</h2>
-                <h4>Nome Azienda</h4>
-                <p>Luogo</p>
-                <a href="#">Altre informazioni</a>
-              </div>
-            </section>
-          </div>
-          <div className="max-h-[60vh] bg-red-500 overflow-scroll transparent-bar">
-            <h2 className="sticky top-0 bg-amber-400 text-center font-extrabold uppercase text-xl">
-              Sent
-            </h2>
-            <section className="p-4">
-              <div className="bg-blue-500 mb-4">
-                <h2>TITOLO OFFERTA</h2>
-                <h4>Nome Azienda</h4>
-                <p>Luogo</p>
-                <a href="#">Altre informazioni</a>
-              </div>
-              <div className="bg-blue-500 mb-4">
-                <h2>TITOLO OFFERTA</h2>
-                <h4>Nome Azienda</h4>
-                <p>Luogo</p>
-                <a href="#">Altre informazioni</a>
-              </div>
-              <div className="bg-blue-500 mb-4">
-                <h2>TITOLO OFFERTA</h2>
-                <h4>Nome Azienda</h4>
-                <p>Luogo</p>
-                <a href="#">Altre informazioni</a>
-              </div>
-              <div className="bg-blue-500 mb-4">
-                <h2>TITOLO OFFERTA</h2>
-                <h4>Nome Azienda</h4>
-                <p>Luogo</p>
-                <a href="#">Altre informazioni</a>
-              </div>
-              <div className="bg-blue-500 mb-4">
-                <h2>TITOLO OFFERTA</h2>
-                <h4>Nome Azienda</h4>
-                <p>Luogo</p>
-                <a href="#">Altre informazioni</a>
-              </div>
-            </section>
-          </div>
-          <div className="max-h-[60vh] bg-red-500 overflow-scroll transparent-bar">
-            <h2 className="sticky top-0 bg-amber-400 text-center font-extrabold uppercase text-xl">
-              Sent
-            </h2>
-            <section className="p-4">
-              <div className="bg-blue-500 mb-4">
-                <h2>TITOLO OFFERTA</h2>
-                <h4>Nome Azienda</h4>
-                <p>Luogo</p>
-                <a href="#">Altre informazioni</a>
-              </div>
-              <div className="bg-blue-500 mb-4">
-                <h2>TITOLO OFFERTA</h2>
-                <h4>Nome Azienda</h4>
-                <p>Luogo</p>
-                <a href="#">Altre informazioni</a>
-              </div>
-              <div className="bg-blue-500 mb-4">
-                <h2>TITOLO OFFERTA</h2>
-                <h4>Nome Azienda</h4>
-                <p>Luogo</p>
-                <a href="#">Altre informazioni</a>
-              </div>
-              <div className="bg-blue-500 mb-4">
-                <h2>TITOLO OFFERTA</h2>
-                <h4>Nome Azienda</h4>
-                <p>Luogo</p>
-                <a href="#">Altre informazioni</a>
-              </div>
-              <div className="bg-blue-500 mb-4">
+              <div className="bg-gray-500 rounded-md p-2 mb-4">
                 <h2>TITOLO OFFERTA</h2>
                 <h4>Nome Azienda</h4>
                 <p>Luogo</p>
