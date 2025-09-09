ff7d8320c38e1bf2c2be0249bbcca9e1dbe539c2
AndreaMargutti <marguttiandrea30@gmail.com>
2025-09-09 15:19:05 +0200

    Add openModal function and update Button component in App: implement a placeholder function for modal handling and pass selection handler to the "Add Job Offer" button.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
 import Button from "./components/Button/Button";
 import Navbar from "./components/Navbar/Navbar";
 import Title from "./components/Title/Title";
+
+// TODO: implement the openModal function for the "add job offers button"
+
+const openModal = () => {};
 
 function App() {
   return (
@@ -10,7 +13,12 @@ function App() {
       <main className="min-h-100 bg-[#3F3F3F]">
         <div className="text-center py-6">
           <Title title="Your Job Offers" />
-          <Button type="button" text="+ Add Job Offer" />
+          <Button
+            type="button"
+            text="+ Add Job Offer"
+            isSelected={false}
+            onSelect={() => openModal()}
+          />
         </div>
       </main>
     </>
