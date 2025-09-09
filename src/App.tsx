47283b0fec3091e2f8000a44ecca7d0045b8ec9d
AndreaMargutti <marguttiandrea30@gmail.com>
2025-09-09 14:09:26 +0200

    Add Title component to App and update main section layout: include a centered title for job offers and adjust main section height for better presentation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Navbar from "./components/Navbar/Navbar";
+import Title from "./components/Title/Title";
 
 function App() {
   return (
     <>
       <Navbar />
-      <main className="h-100 bg-[#F3F6FA]"></main>
+      <main className="min-h-100 bg-[#F3F6FA]">
+        <div className="text-center py-6">
+          <Title title="Your Job Offers" />
+        </div>
+      </main>
     </>
   );
 }
