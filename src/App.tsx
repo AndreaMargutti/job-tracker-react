34a7080632d8ed26d9ca3b4a4262f32d4c5224c7
AndreaMargutti <marguttiandrea30@gmail.com>
2025-09-16 15:20:06 +0200

    feat: implement job sections and add JobTab component for job offer display

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,22 @@ import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Title from "./components/Title/Title";
 
+const jobSections = [
+  { id: 1, title: "Sent" },
+  { id: 2, title: "Interview" },
+  { id: 3, title: "Offer" },
+];
+
+function sections() {
+  return jobSections.map((section) => (
+    <div className="max-h-[60vh] border-4 rounded-md text-accent-azure overflow-scroll transparent-bar">
+      <h2 className="bg-base-gray sticky top-0 text-center py-4 font-extrabold uppercase text-xl">
+        {section.title}
+      </h2>
+    </div>
+  ));
+}
+
 // TODO: implement the openModal function for the "add job offers button"
 
 const openModal = () => {};
@@ -21,24 +37,8 @@ function App() {
             onSelect={() => openModal()}
           />
         </div>
-        {/* Start of Table section */}
         <section className="grid grid-cols-3 gap-8 px-8 pb-8">
-          {/*  Start of SENT section */}
-          <div className="max-h-[60vh] border-4 rounded-md text-accent-azure overflow-scroll transparent-bar">
-            <h2 className="bg-base-gray sticky top-0 text-center py-4 font-extrabold uppercase text-xl">
-              Sent
-            </h2>
-            <section className="p-4 pt-0">
-              <div className="bg-accent-gray rounded-md p-2 mb-4 shadow-md/45 shadow-accent-azure">
-                <h2 className="font-bold text-lg uppercase ">TITOLO OFFERTA</h2>
-                <div className="flex gap-2 mb-2">
-                  <h4>Nome Azienda</h4>
-                  <p>Luogo</p>
-                </div>
-                <a href="#">Altre informazioni</a>
-              </div>
-            </section>
-          </div>
+          {sections()}
         </section>
       </main>
       <footer className="bg-base text-white p-8">
---
+++ src/components/JobTab/JobTab.tsx
@@ -0,0 +1,14 @@
+export default function JobTab() {
+  return (
+    <section className="p-4 pt-0">
+      <div className="bg-accent-gray rounded-md p-2 mb-4 shadow-md/45 shadow-accent-azure">
+        <h2 className="font-bold text-lg uppercase ">TITOLO OFFERTA</h2>
+        <div className="flex gap-2 mb-2">
+          <h4>Nome Azienda</h4>
+          <p>Luogo</p>
+        </div>
+        <a href="#">Altre informazioni</a>
+      </div>
+    </section>
+  );
+}
