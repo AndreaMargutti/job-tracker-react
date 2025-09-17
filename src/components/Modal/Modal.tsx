a474a334be9b66c3d7debddf3805e6a08e48960b
AndreaMargutti <marguttiandrea30@gmail.com>
2025-09-17 15:41:13 +0200

    fix: improve styling of form elements in Modal component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -9,7 +9,7 @@ export default function Modal({ isOpen }: ModalProps) {
       <div className="border-4 border-accent-azure rounded-lg p-4 min-w-xl absolute top-[50%] left-[50%] -translate-[50%]">
         <Title title="Insert New Offer" />
         <form action="" method="POST">
-          <div>
+          <div className="p-4 border-2 border-accent-azure rounded-lg">
             <label htmlFor="">State</label>
             <select name="" id="">
               <option value="Sent">Sent</option>
@@ -17,17 +17,21 @@ export default function Modal({ isOpen }: ModalProps) {
               <option value="Offer">Offers</option>
             </select>
           </div>
-          <div>
+          <div className="border-2 border-accent-azure rounded-lg p-4 my-4">
             <label htmlFor="">Job Title</label>
             <input type="text" />
           </div>
-          <div>
-            <label htmlFor="">Company</label>
-            <input type="text" />
-            <label htmlFor="">Place</label>
-            <input type="text" name="" id="" />
+          <div className="flex gap-4">
+            <div className="border-2 border-accent-azure rounded-lg p-4 my-4">
+              <label htmlFor="">Company</label>
+              <input type="text" />
+            </div>
+            <div className="border-2 border-accent-azure rounded-lg p-4 my-4">
+              <label htmlFor="">Place</label>
+              <input type="text" name="" id="" />
+            </div>
           </div>
-          <div>
+          <div className="border-2 border-accent-azure rounded-lg p-4 my-4 min-h-50">
             <label htmlFor="">Job Description</label>
             <input type="text" />
           </div>
